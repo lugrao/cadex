b4ec75541db185788477327fc62f7a1638a56966
lugrao <56491057+lagoru-q@users.noreply.github.com>
2020-05-08 12:54:31 -0300

    se cambia mayúscula del título

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
       <div id="navbar-0"></div>
       <nav className="navbar">
         <span id="navbar-titulo" className="navbar-brand mb-0 h1">
-          cadex (es por <i>cadáver exquisito</i>) (beta)
+          Cadex (es por <i>cadáver exquisito</i>) (beta)
         </span>
         <span className="navbar-text">
           Escribí una historia junto a otra gente.
