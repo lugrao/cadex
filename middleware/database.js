1bbbcd2bb8be23d407fbae9eca278efd573d53fd
lugrao <56491057+lagoru-q@users.noreply.github.com>
2020-05-08 13:12:32 -0300

    se cambia uri de db

--- middleware/database.js
+++ middleware/database.js
@@ -9,7 +9,7 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 async function database(req, res, next) {
     if (!client.isConnected()) await client.connect();
     req.dbClient = client;
-    req.db = client.db("cadexDBbackup");
+    req.db = client.db("cadexDB");
     return next();
 }
 
