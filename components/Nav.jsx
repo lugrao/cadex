757768df2e1f8933333ea3a394c1fd70c7daf4c0
lugrao <elugrao@gmail.com>
2020-07-16 15:49:26 -0300

    actualiza url de app en vercel.json

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export default function Nav({ user, loading }) {
+  console.log(user);
   return (
     <nav id="navbar">
       <h1>Cadex</h1>
@@ -9,12 +10,30 @@ export default function Nav({ user, loading }) {
       </Link>
       {!loading &&
         (user ? (
-          <p>usuario conectado</p>
+          <Link href="api/auth/logout">
+            <a>Logout</a>
+          </Link>
         ) : (
           <Link href="api/auth/login">
             <a>Login</a>
           </Link>
         ))}
+      {user && <div className="usuario">
+        <p>{user.name}</p>
+        <img src={user.picture} /></div>}
+      <style jsx>{`
+        .usuario {
+          display:grid;
+          grid-template-columns: 1fr 1fr;
+        }
+        .usuario p {
+          justify-self:right;
+          padding-right: 10px;
+        }
+        img{
+          width:40px;
+        }
+      `}</style>
     </nav>
   );
 }
