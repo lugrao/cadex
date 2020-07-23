f16eec8eefa15347784a0555f9d24211cc637757
lugrao <elugrao@gmail.com>
2020-07-23 17:05:15 -0300

    mejora UI de Redactar

--- components/Redactar.jsx
+++ components/Redactar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Textarea, Box, Button } from "@chakra-ui/core";
 
 export default function Redactar(props) {
   const [escribir, setEscribir] = useState(false);
@@ -35,32 +36,43 @@ export default function Redactar(props) {
 
   return (
     <div id="redactar" className="redactar">
-      <textarea
-        placeholder="Escribí el siguiente capítulo..."
-        rows={escribir ? "4" : "2"}
-        maxLength="300"
-        className={pocosCaracteres ? "form-control is-invalid" : "form-control"}
-        name="contenido"
-        onClick={!escribir ? desplegarTextArea : null}
-        onChange={actualizarContenido}
-        value={textAreaValue}
-      ></textarea>
+      <Box
+        display="grid"
+        m="10px 7px"
+        maxW="30rem"
+        p={5}
+        // shadow="sm"
+        // borderWidth="1px"
+        id={props.id}
+      >
+        <Textarea
+          size="lg"
+          placeholder="Escribí el siguiente capítulo..."
+          rows={escribir ? "4" : "2"}
+          maxLength="300"
+          className={
+            pocosCaracteres ? "form-control is-invalid" : "form-control"
+          }
+          name="contenido"
+          onClick={!escribir ? desplegarTextArea : null}
+          onChange={actualizarContenido}
+          value={textAreaValue}
+        ></Textarea>
+        <Button
+          mt="7px"
+          variantColor="green"
+          id="btn-publicar"
+          className="btn btn-warning"
+          onClick={publicar}
+        >
+          Publicar
+        </Button>
+      </Box>
+
       {pocosCaracteres && (
         <div className="pocos-caracteres">
           Mínimo 10 caracteres. Dale que vos tenés talento.
         </div>
-      )}
-      {escribir && (
-        <>
-          <br />
-          <button
-            id="btn-publicar"
-            className="btn btn-warning"
-            onClick={publicar}
-          >
-            Publicar
-          </button>
-        </>
       )}
     </div>
   );
