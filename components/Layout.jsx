83940c794d2ad434602988c4bdf074efd29e98f5
lugrao <elugrao@gmail.com>
2021-05-28 16:31:11 -0300

    agrega favicon y modifica title en Head

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,11 +5,12 @@ export default function Layout({ children }) {
   return (
     <ThemeProvider>
       <Head>
-        <title>cadex 0.2</title>
+        <title>cadex</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link rel="shortcut icon" href="/toiletpaper.svg" />
       </Head>
       <CSSReset />
       <main id="main">{children}</main>
